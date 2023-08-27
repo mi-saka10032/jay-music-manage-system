2e54de9b9c9fa0e0691220dcd336f074e27b3d6e
misaka10032 <misaka10032@aliyun.com>
2023-08-27 15:54:26 +0800

    fix: 修复打包类型报错

--- src/interface.ts
+++ src/interface.ts
@@ -1,10 +1,15 @@
 import '@midwayjs/core';
+import '@midwayjs/ws';
 import { UserContext } from './common/UserContext';
 
 declare module '@midwayjs/core' {
   interface Context {
     userContext: UserContext;
+  }
+}
 
+declare module '@midwayjs/ws' {
+  interface Context {
     socketId: string;
   }
 }
