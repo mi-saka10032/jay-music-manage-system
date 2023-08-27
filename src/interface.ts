70c4d6c8623a10a7f3021022786febd54416be1c
misaka10032 <misaka10032@aliyun.com>
2023-08-27 22:45:46 +0800

    fix: 恢复interface.ts

--- src/interface.ts
+++ src/interface.ts
@@ -1,15 +1,9 @@
 import '@midwayjs/core';
-import '@midwayjs/ws';
 import { UserContext } from './common/UserContext';
 
 declare module '@midwayjs/core' {
   interface Context {
     userContext: UserContext;
-  }
-}
-
-declare module '@midwayjs/ws' {
-  interface Context {
     socketId: string;
   }
 }
